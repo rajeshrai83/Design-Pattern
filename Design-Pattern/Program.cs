3e83a921c6f0a85865ef36328f0a269c8999dcfe
Rajesh Rai <rajesh_rai83@yahoo.com>
2018-05-14 23:03:52 +0530

    First commit for singleton design pattern

---
+++ Design-Pattern/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton obj = Singleton.Instance;
+
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        Singleton()
+        {
+            Console.WriteLine("I am in Singleton design pattern");
+            Console.ReadLine();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+
+                // Thread safe version
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+    public sealed class NetSingleton
+    {
+        private static readonly Lazy<NetSingleton> lazy =
+            new Lazy<NetSingleton>(() => new NetSingleton());
+
+        public static NetSingleton Instance { get { return lazy.Value; } }
+
+        private NetSingleton()
+        {
+        }
+    }
+}
